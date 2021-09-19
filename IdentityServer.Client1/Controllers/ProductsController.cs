75b75f8ed5f7a0acbf7ff609593eda3cc394cb77
Vedat ERMIS <vedatermis@hotmail.com>
2021-09-19 16:14:28 +0300

    commit

---
+++ IdentityServer.Client1/Controllers/ProductsController.cs
@@ -0,0 +1,56 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace IdentityServer.Client1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public ProductsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            var disco = await httpClient.GetDiscoveryDocumentAsync("https://localhost:5001");
+            if (disco.IsError)
+            {
+                //Loglama
+            }
+
+            var clientCredentialsTokenRequest = new ClientCredentialsTokenRequest();
+            clientCredentialsTokenRequest.ClientId = _configuration["Client:ClientId"];
+            clientCredentialsTokenRequest.ClientSecret = _configuration["Client:ClientSecret"];
+            clientCredentialsTokenRequest.Address = disco.TokenEndpoint;
+
+            TokenResponse token = await httpClient.RequestClientCredentialsTokenAsync(clientCredentialsTokenRequest);
+
+            if (token.IsError)
+            {
+                //Loglama
+            }
+            
+            httpClient.SetBearerToken(token.AccessToken);
+
+            var response = await httpClient.GetAsync("http://localhost:5004/api/products/GetProducts");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                
+            }
+            
+            return View();
+        }
+    }
+}
